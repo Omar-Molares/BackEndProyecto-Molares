ab22ebfe87f04c89923aab1b1ac7b9584e5a1021
Omar Molares <omarmolares1984@gmail.com>
2025-04-07 19:50:29 -0300

    first commit

---
+++ src/CartManager.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+class CartManager {
+    constructor(){
+        this.path = './src/cart.json';
+    }
+
+    generateNewId = () => {
+        if(carts.length > 0){
+            return carts[carts.length -1].id + 1;
+        }else{
+            return 1;
+        }
+    }
+
+    addCart =async () => {
+        const cartsJson = await fs.promises.readFile(this.path, "utf-8")
+        const carts = JSON.parse(cartsJson)
+        
+        const id = this.generateNewId(carts)
+        carts.push({ id, products: []})
+
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2), "utf-8");
+
+        return carts;
+    }
+}
+
+export default CartManager;
